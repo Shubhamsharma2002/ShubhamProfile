03999fe520a599e24129325608254bf54a8fdec6
shubham sharma <shubhamtech004@gmail.com>
2025-05-23 23:28:29 +0530

    footer layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return(
       <main className="flex min-h-screen flex-col bg-[#121212]  ">
         <Navbar/>
-          <HeroSection/>
+          <div className="flex-grow">
+        <HeroSection />
+      </div>
+          <Footer/>
       </main>
   )
 }
