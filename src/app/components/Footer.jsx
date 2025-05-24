61c817fac067bbe2a1e45013080f5c73e833c560
shubham sharma <shubhamtech004@gmail.com>
2025-05-24 22:37:46 +0530

    footer section

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer =()=> {
   return (
-    <div className='text-white'>Footer</div>
+    <div className='text-white text-center m-1'>All right Reserved with || SHUBHAM SHARMA ||</div>
   )
 }
 
