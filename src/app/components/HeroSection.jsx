a63a615081a7234938183183329be4defb28396d
shubham sharma <shubhamtech004@gmail.com>
2025-05-24 22:39:23 +0530

    intro section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HeroSection =()=> {
   return (
-    <div className='text-white'>HeroSection</div>
+    <div className='text-white flex justify-around m-1'>
+        <div className="font-bold ">intro</div>
+        <div className="">image</div>
+    </div>
   )
 }
 
