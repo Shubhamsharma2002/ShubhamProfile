5e04672b85f1f10e3ca51e94b020d8d6f8abb9c0
shubham sharma <shubhamtech004@gmail.com>
2025-05-24 22:35:35 +0530

    navrbar done

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar =()=> {
   return (
-    <div className='text-white bg-amber-300 p-5 items-center'>Navbar</div>
+    <div className='text-white bg-amber-300 p-5 items-center flex justify-around'>
+      <div className="font-bold italic text-4xl">Shubham</div>
+      <div className="flex gap-4 cursor-pointer">
+        <p>Intro</p>
+        <p>Skills</p>
+        <p>project</p>
+        <p>education</p>
+      </div>
+    </div>
   )
 }
 
