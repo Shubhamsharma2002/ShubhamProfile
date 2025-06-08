d0942c18de93cd6d960dedff674dc69a74a504be
shubham sharma <shubhamtech004@gmail.com>
2025-06-08 23:09:07 +0530

    project setup

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Project() {
+const Project =()=> {
   return (
     <div>
       <div className="">Project</div>
--- src/app/components/Skilss.jsx
+++ src/app/components/Skilss.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Skils() {
+const Skils =() =>{
   return (
     <div>
       <div className="">text</div>
